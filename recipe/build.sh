72fabfe47ddd5dd5a59114f932cbd814a3be314f
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-02 11:32:49 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 go mod init github.com/svent/sift
 go mod tidy -e
 go mod vendor -e
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files --ignore=github.com/svent/sift
